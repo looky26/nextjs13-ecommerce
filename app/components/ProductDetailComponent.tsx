05353a5cdb3beb3c9acbc29b035e97ff33d628e7
looky26 <vajearvin03@gmail.com>
2023-02-25 23:20:13 +0800

    first commit

--- app/components/ProductDetailComponent.tsx
+++ app/components/ProductDetailComponent.tsx
@@ -20,7 +20,7 @@ const ProductDetailComponent = ({ productSlug }: any) => {
     <div>
       <div className="flex  max-w-7xl mx-auto bg-zinc-900">
         {/* left */}
-        <img src={urlFor(productSlug.productImage).url()} alt="" className="w-96 h-96" />
+        <img src={urlFor(productSlug.productImage)?.url()} alt="" className="w-96 h-96" />
 
         {/* right */}
         <div className="ml-20 space-y-3 pt-5">
