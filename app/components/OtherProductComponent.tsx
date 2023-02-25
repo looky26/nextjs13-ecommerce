8c93f453747766ebf918182ff77fcdfc83de0ea7
looky26 <vajearvin03@gmail.com>
2023-02-26 06:03:09 +0800

    modified otherprodcomponent

--- app/components/OtherProductComponent.tsx
+++ app/components/OtherProductComponent.tsx
@@ -9,13 +9,13 @@ const OtherProductComponent = ({ products }: any) => {
     <div className="flex justify-evenly items-center flex-wrap gap-y-2">
       {products.map((item:any) => (
         <Link href={"/product/" + item.slug.current}>
-          <div className="flex flex-col items-center bg-zinc-700 p-2 rounded-md">
+          <div className="flex flex-col items-center bg-zinc-700 p-2 rounded-md w-96 h-80">
             <img
               src={urlFor(item.productImage).url()}
               alt=""
               className="h-60 w-60"
             />
-            <p>{item.item}</p>
+            <p className="text-center">{item.item}</p>
             <p>{item.price}</p>
           </div>
         </Link>
